7233841ea1c5873fbe5817fddf3c5df689694a9b
Hank Brigham <hhbrigham5@gmail.com>
2022-10-04 12:29:53 -0600

    chore: removing eslint and optimizing

--- src/declarations/delastic_search/delastic_search.did.js
+++ src/declarations/delastic_search/delastic_search.did.js
@@ -1,25 +1,40 @@
 export const idlFactory = ({ IDL }) => {
-  const RecordAttributes = IDL.Vec(
-    IDL.Tuple(IDL.Text, IDL.Variant({ text: IDL.Opt(IDL.Text) })),
-  );
+  const AttributeValuePrimitive = IDL.Variant({
+    int: IDL.Int,
+    float: IDL.Float64,
+    bool: IDL.Bool,
+    text: IDL.Text,
+  });
+  const AttributeValue = IDL.Variant({
+    int: IDL.Int,
+    float: IDL.Float64,
+    tuple: IDL.Vec(AttributeValuePrimitive),
+    blob: IDL.Vec(IDL.Nat8),
+    bool: IDL.Bool,
+    text: IDL.Text,
+    arrayBool: IDL.Vec(IDL.Bool),
+    arrayText: IDL.Vec(IDL.Text),
+    arrayInt: IDL.Vec(IDL.Int),
+    arrayFloat: IDL.Vec(IDL.Float64),
+  });
+  const RecordAttributes = IDL.Vec(IDL.Tuple(IDL.Text, AttributeValue));
   const Record = IDL.Record({
     id: IDL.Text,
     attributes: RecordAttributes,
     entityType: IDL.Text,
   });
-  const Error = IDL.Variant({
-    NotFound: IDL.Null,
-    NotAuthorized: IDL.Null,
-    AlreadyExists: IDL.Null,
-    InternalError: IDL.Null,
-    Forbidden: IDL.Null,
-    BadRequest: IDL.Null,
+  const Result = IDL.Variant({
+    ok: IDL.Vec(Record),
+    err: IDL.Vec(Record),
   });
-  const Result = IDL.Variant({ ok: IDL.Vec(Record), err: Error });
   return IDL.Service({
-    addRecord: IDL.Func([Record, IDL.Vec(IDL.Text), IDL.Vec(IDL.Text)], [], []),
     queryIndex: IDL.Func([IDL.Text, IDL.Text], [Result], ['query']),
     removeRecord: IDL.Func([IDL.Text, IDL.Vec(IDL.Text)], [], []),
+    updateRecord: IDL.Func(
+      [Record, IDL.Vec(IDL.Text), IDL.Vec(IDL.Text)],
+      [],
+      [],
+    ),
   });
 };
 export const init = ({ IDL }) => {
---
+++ src/types.mo
@@ -0,0 +1,54 @@
+import TrieMap "mo:base/TrieMap";
+
+module {
+	public type AttributeValuePrimitive = {
+    #text: Text;
+    #int: Int;
+    #bool: Bool;
+    #float: Float;
+  };
+
+  public type AttributeValueBlob = {
+    #blob: Blob;
+  };
+
+  /// An AttributeValue can be an array of AttributeValuePrimitive (tuple type)
+  public type AttributeValueTuple = {
+    #tuple: [AttributeValuePrimitive];
+  };
+
+  /// An AttributeValue can be an array of any single one the primitive types (i.e. [Int])
+  public type AttributeValueArray = {
+    #arrayText: [Text];
+    #arrayInt: [Int];
+    #arrayBool: [Bool];
+    #arrayFloat: [Float];
+  };
+
+	public type AttributeValue = 
+    AttributeValuePrimitive or 
+    AttributeValueBlob or
+    AttributeValueTuple or
+    AttributeValueArray;
+
+	public type RecordAttributes = [(Text, AttributeValue)];
+
+	public type Record = {
+		id : Text;
+		entityType : Text;
+		attributes : RecordAttributes;
+	};
+
+	type RecordList = [Record];
+
+	public type Index = TrieMap.TrieMap<Text, RecordList>;
+
+	public type Error = {
+		#NotFound;
+		#AlreadyExists;
+		#NotAuthorized;
+		#BadRequest;
+		#InternalError;
+		#Forbidden;
+	};
+}
